943a06f826d1fd06108d2d1b8b96a9155cea0147
ZeePLA <gokhanworkacc@gmail.com>
2024-07-12 20:44:36 +0300

    added shadow of required environmental variable to example.env file to show what required.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Fetching fx rates from EVDS
 
+## Time-Sheet
+
+1- This project is using EVDS API to query fx rates from TCMB(Turkish Central Bank).
+
+2- I also added a twitter feature so I can tweet by calling an endpoint on the backend. Used twitter-api-v2 on this. For now it is static and tweets a string.
+
+3- Planning on merging these two features so that I could timely tweet about currencies and stuff.
+
 ### Data Group: FX Rates:
 
 - Seri: (USD) ABD Doları (Döviz Alış)- TP.DK.USD.A,
@@ -7,3 +15,8 @@
 - Seri: (CHF) İsviçre Frangı (Döviz Alış)- TP.DK.CHF.A,
 - Seri: (GBP) İngiliz Sterlini (Döviz Alış)- TP.DK.GBP.A,
 - Seri: (JPY) Japon Yeni (Döviz Alış)- TP.DK.JPY.A
+
+###
+
+choco-bot
+@gokhan126154728
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,16 +1,34 @@
 import express from "express";
 const router = express.Router();
-import { fetchAndStoreData, fetchedData } from "../controllers/dataFetcher.js";
+import { fetchData, responseData } from "../controllers/dataFetcher.js";
+import { url } from "../controllers/urlController.js";
+import tweet from "../controllers/twitterUtility.js";
 
 // Test-API's
 router.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
-// API-Router
+// API-Routers
 router.get("/query", async (req, res) => {
-  await fetchAndStoreData();
-  res.json(fetchedData);
+  await fetchData(url);
+  res.json(responseData);
+});
+
+// Constructive-API
+router.get("/query/:series/:startdate/:enddate/:type", async (req, res) => {
+  const { series, startdate, enddate, type } = req.params;
+  const reconstructedUrl = `https://evds2.tcmb.gov.tr/service/evds/series=${series}&startDate=${startdate}&endDate=${enddate}&type=${type}`;
+  await fetchData(reconstructedUrl);
+  res.json(responseData);
+});
+
+// Tweetter-Test-API
+router.post("/tweet/hello-world", async (req, res) => {
+  await tweet();
+  res
+    .status(200)
+    .send("Meris tweeted with the choco-bot....\n choco-bot giggles...");
 });
 
 // Endpoint-NotFound
